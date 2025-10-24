d2d548041326536972ff09910a2e7685bdfb5545
harshyadavDeveloper <harshyadav5939@gmail.com>
2025-10-24 11:47:44 +0530

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,24 @@
 import express from "express";
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.route.js';
+import path from 'path';
 dotenv.config();
 
 const app = express();
+const __driname = path.resolve();
 console.log(process.env.PORT);
 const PORT = process.env.PORT || 3333;
 
 app.use("/api/auth", authRoutes);
+
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__driname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__driname, "../frontend","dist", "index.html"));
+    })
+}
 
 
 
