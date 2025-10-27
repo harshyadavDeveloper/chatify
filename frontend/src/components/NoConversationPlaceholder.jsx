9d76ddad3b13540d519c65f153860519eaed64f5
harshyadavDeveloper <harshyadav5939@gmail.com>
2025-10-27 17:36:01 +0530

    chat-page-setup

---
+++ frontend/src/components/NoConversationPlaceholder.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NoConversationPlaceholder() {
+  return (
+    <div>
+      no conversation place holder
+    </div>
+  )
+}
+
+export default NoConversationPlaceholder
