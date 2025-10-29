b492c1e49ed911bf52d825cd6523ebd42ffa06ef
harshyadavDeveloper <harshyadav5939@gmail.com>
2025-10-29 17:43:36 +0530

    online users implemented

--- frontend/src/components/ChatHeader.jsx
+++ frontend/src/components/ChatHeader.jsx
@@ -6,7 +6,7 @@ import { useAuthStore } from "../store/useAuthStore";
 function ChatHeader() {
   const { selectedUser, setSelectedUser } = useChatStore();
   const { onlineUsers } = useAuthStore();
-  // const isOnline = onlineUsers.includes(selectedUser._id);
+  const isOnline = onlineUsers.includes(selectedUser._id);
 
   useEffect(() => {
     const handleEscKey = (event) => {
@@ -25,8 +25,8 @@ function ChatHeader() {
    border-slate-700/50 max-h-[84px] px-6 flex-1"
     >
       <div className="flex items-center space-x-3">
-        {/* <div className={`avatar ${isOnline ? "online" : "offline"}`}> */}
-         <div className={`avatar online"}`}>
+        <div className={`avatar ${isOnline ? "online" : "offline"}`}>
+         {/* <div className={`avatar online"}`}> */}
           <div className="w-12 rounded-full">
             <img src={selectedUser.profilePic || "/avatar.png"} alt={selectedUser.fullName} />
           </div>
@@ -35,7 +35,7 @@ function ChatHeader() {
         <div>
           <h3 className="text-slate-200 font-medium">{selectedUser.fullName}</h3>
           {/* <p className="text-slate-400 text-sm">{isOnline ? "Online" : "Offline"}</p> */}
-          <p className="text-slate-400 text-sm">{ "Online"}</p>
+          <p className="text-slate-400 text-sm">{isOnline? "Online": "Offline"}</p>
         </div>
       </div>
 
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,18 +3,23 @@ import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-export const useAuthStore = create((set) => ({
+const BASE_URL =
+  import.meta.env.MODE === "development" ? "http://localhost:3000" : "/";
+
+export const useAuthStore = create((set, get) => ({
   authUser: null,
   isCheckingAuth: true,
   isSigningUp: false,
   isLoggingIn: false,
   socket: null,
+  onlineUsers: [],
 
   checkAuth: async () => {
     try {
       const res = await axiosInstance.get("/auth/check");
       console.log("response: ", res.data, res.status);
       set({ authUser: res.data });
+      get().connectSocket();
     } catch (error) {
       console.log("Error in authCheck: ", error);
       set({ authUser: null });
@@ -29,6 +34,7 @@ export const useAuthStore = create((set) => ({
       const res = await axiosInstance.post("/auth/signup", data);
       set({ authUser: res.data });
       toast.success("Account created successfully!");
+      get().connectSocket();
     } catch (error) {
       toast.error(error.response.data.message);
     } finally {
