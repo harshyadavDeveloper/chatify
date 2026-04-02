44a52dab9f139ed72c7f0c154542f87f48a30922
harshyadavDeveloper <harshyadav5939@gmail.com>
2026-04-02 11:42:14 +0530

    added new origin

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: [ENV.CLIENT_URL],
+    origin: [ENV.CLIENT_URL, ENV.VITE_API_BASE_URL],
     credentials: true,
   },    
 });
