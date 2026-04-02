f9c88d14e79bd6248108aae5a913c125dd35656c
harshyadavDeveloper <harshyadav5939@gmail.com>
2026-04-02 16:30:55 +0530

    minor changes

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -14,7 +14,7 @@ export const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000, // MS
     httpOnly: true, // prevent XSS attacks: cross-site scripting
-    sameSite: "strict", // CSRF attacks
+    sameSite: "none", // CSRF attacks
     secure: ENV.JWT_SECRET === "development" ? false : true,
   });
 
